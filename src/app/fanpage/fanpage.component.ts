3e45a42bc80c9823bdfcda8ca0864d5d94726367
anna <anna.janusz@loycon.pl>
2021-07-14 22:15:15 +0200

    Added global file with globals variables

--- src/app/fanpage/fanpage.component.ts
+++ src/app/fanpage/fanpage.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {GlobalVariable} from '../../globals';
 
 @Component({
   selector: 'app-fanpage',
@@ -6,6 +7,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./fanpage.component.css']
 })
 export class FanpageComponent implements OnInit {
+  baseApiUrl = GlobalVariable.FAN_PAGE_URL;
 
   constructor() { }
 
---
+++ src/globals.ts
@@ -0,0 +1,3 @@
+export const GlobalVariable = Object.freeze({
+  FAN_PAGE_URL: 'https://www.facebook.com/Gor%C4%85cy-sok-pomara%C5%84czowy-100123082361146',
+});
