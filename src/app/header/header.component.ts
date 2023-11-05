7a29949f8bc385b67a4cfece945bd40ea950cad2
Anna Pajor <apajor@mnfro.com>
2023-11-05 22:27:25 +0100

    Updated styles

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -3,18 +3,17 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'gs-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.css'],
+  styleUrls: ['./header.component.scss'],
 })
-export class HeaderComponent implements OnInit {
+export class HeaderComponent {
   scrollTo(id: string): void {
     const el: HTMLElement | null = document.getElementById(id);
-    if (el) {
-      setTimeout(
-        () => el.scrollIntoView({ behavior: 'smooth', block: 'center', inline: 'nearest' }),
-        0
-      );
+    if (!el) {
+      return;
     }
+    setTimeout(
+      () => el.scrollIntoView({ behavior: 'smooth', block: 'center', inline: 'nearest' }),
+      0
+    );
   }
-
-  ngOnInit() {}
 }
